42e26dc3b72b59342124d3dcc02127780e59dc5c
Louis <louis@Louiss-Mac-mini.local>
2019-08-21 12:43:59 -0400

    Fetched repos in api.js and added to Popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { fetchPopularRepos } from '../utils/api';
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -23,17 +24,44 @@ function LanguagesNav({ selected, onUpdateLanguage }) {
 
 export default class Popular extends React.Component {
   state = {
-    selectedLanguage: 'All'
+    selectedLanguage: 'All',
+    repos: null,
+    error: null
   };
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage)
+  }
 
   updateLanguage = selectedLanguage => {
     this.setState({
-      selectedLanguage
+      selectedLanguage,
+      error: null,
+      repos: null
     });
+
+    fetchPopularRepos(selectedLanguage)
+      .then(repos =>
+        this.setState({
+          repos,
+          error: null
+        })
+      )
+      .catch(() => {
+        console.warn('Error fetching repos: ', error);
+
+        this.setState({
+          error: `There was an error fetching the repositories`
+        });
+      });
   };
 
+  isLoading() {
+    return this.state.repos === null && this.state.error === null;
+  }
+
   render() {
-    const { selectedLanguage } = this.state;
+    const { selectedLanguage, repos, error } = this.state;
 
     return (
       <>
@@ -41,12 +69,18 @@ export default class Popular extends React.Component {
           selected={selectedLanguage}
           onUpdateLanguage={this.updateLanguage}
         />
+
+        {this.isLoading() && <p>LOADING</p>}
+
+        {error && <p>{error}</p>}
+
+        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
       </>
-    )
+    );
   }
 }
 
 LanguagesNav.propTypes = {
   selected: PropTypes.string.isRequired,
   onUpdateLanguage: PropTypes.func.isRequired
-}
+};
