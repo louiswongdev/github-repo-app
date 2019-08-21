da76b2e52c8eb9d069ae247d550092270b186ecd
Louis <louis@Louiss-Mac-mini.local>
2019-08-21 12:05:51 -0400

    Moved nav into its own LanguagesNav component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+
+function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+  return (
+    <ul className="flex-center">
+      {languages.map(language => (
+        <li key={language}>
+          <button
+            onClick={() => onUpdateLanguage(language)}
+            style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
+            className="btn-clear nav-link"
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default class Popular extends React.Component {
   state = {
@@ -12,26 +33,20 @@ export default class Popular extends React.Component {
   };
 
   render() {
-    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+    const { selectedLanguage } = this.state;
 
     return (
-      <ul className="flex-center">
-        {languages.map(language => (
-          <li key={language}>
-            <button
-              onClick={() => this.updateLanguage(language)}
-              style={
-                language === this.state.selectedLanguage
-                  ? { color: 'rgb(187, 46, 31)' }
-                  : null
-              }
-              className="btn-clear nav-link"
-            >
-              {language}
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
+      <>
+        <LanguagesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+      </>
+    )
   }
+}
+
+LanguagesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired
 }
