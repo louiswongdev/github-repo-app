7302b6bab28561b410e92e9c704fe7bb5314ec96
Louis <louis@Louiss-Mac-mini.lan>
2019-08-27 15:52:00 -0400

    Added reuseable Card component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { fetchPopularRepos } from '../utils/api';
+
+import Card from './Card';
+
 import {
   FaUser,
   FaStar,
@@ -43,19 +46,14 @@ function ReposGrid({ repos }) {
         const { login, avatar_url } = owner;
 
         return (
-          <li key={html_url} className="repo bg-light">
-            <h4 className="header-lg center-text">#{index + 1}</h4>
-            <img
-              src={avatar_url}
-              alt={`Avatar for ${login}`}
-              className="avatar"
-            />
-            <h2 className="center-text">
-              <a href={html_url} className="link">
-                {login}
-              </a>
-            </h2>
-            <ul className="card-list">
+          <li key={html_url}>
+            <Card
+              header={`#${index + 1}`}
+              avatar={avatar_url}
+              href={html_url}
+              name={login}
+            >
+              <ul className="card-list">
               <li>
                 <FaUser color="rgb(255, 191, 116)" size={22} />
                 <a href={`https://github.com/${login}`}>{login}</a>
@@ -73,6 +71,7 @@ function ReposGrid({ repos }) {
                 {open_issues.toLocaleString()} open
               </li>
             </ul>
+            </Card>          
           </li>
         );
       })}
@@ -136,7 +135,7 @@ export default class Popular extends React.Component {
 
         {this.isLoading() && <p>LOADING</p>}
 
-        {error && <p>{error}</p>}
+        {error && <p className='center-text error'>{error}</p>}
 
         {repos[selectedLanguage] && (
           <ReposGrid repos={repos[selectedLanguage]} />
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,5 +1,9 @@
 import React from 'react'
+
 import { battle } from '../utils/api';
+import Card from './Card';
+
+import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
 
 export default class Results extends React.Component {
   state = {
@@ -29,10 +33,88 @@ export default class Results extends React.Component {
   }
 
   render() {
+    const { winner, loser, error, loading } = this.state;
+    
+    if(loading === true) {
+      return <p>LOADING</p>
+    }
+
+    if(error) {
+      return (
+        <p className="center-text error">{error}</p>
+      )
+    }
+
     return (
-      <div>
-        Results
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+      <div className='grid space-around container-sm'>
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Winner'}
+          subheader={`Score: ${winner.score.toLocaleString()}`}
+          avatar={winner.profile.avatar_url}
+          href={winner.profile.html_url}
+          name={winner.profile.login}
+        >
+          <ul className='card-list'>
+            <li>
+              <FaUser color='rgb(239, 115, 115)' size={22} />
+              {winner.profile.name !== null ? `${winner.profile.name}` : 'N/A'}
+            </li>
+            {winner.profile.location && (
+              <li>
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {winner.profile.location}
+              </li>
+            )}
+            {winner.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {winner.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {winner.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+              {winner.profile.following.toLocaleString()} following
+            </li>
+          </ul>
+        </Card>
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Loser'}
+          subheader={`Score: ${loser.score.toLocaleString()}`}
+          avatar={loser.profile.avatar_url}
+          name={loser.profile.login}
+          href={loser.profile.html_url}
+        >
+          <ul className='card-list'>
+            <li>
+              <FaUser color='rgb(239, 115, 115)' size={22} />
+              {loser.profile.name !== null ? `${loser.profile.name}` : 'N/A'}
+            </li>
+            {loser.profile.location && (
+              <li>
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {loser.profile.location}
+              </li>
+            )}
+            {loser.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {loser.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {loser.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+              {loser.profile.following.toLocaleString()} following
+            </li>
+          </ul>
+        </Card>
       </div>
     )
   }
