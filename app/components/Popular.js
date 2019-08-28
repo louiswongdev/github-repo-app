eaaa31b14bd14b7e01f281e3e10ac8dccb31e0c6
Louis <louis@Louiss-Mac-mini.lan>
2019-08-27 23:13:45 -0400

    Added Tooltip and withHover HOC components

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -11,6 +11,7 @@ import {
   FaCodeBranch,
   FaExclamationTriangle
 } from 'react-icons/fa';
+import Tooltip from './Tooltip';
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -56,8 +57,10 @@ function ReposGrid({ repos }) {
             >
               <ul className="card-list">
               <li>
-                <FaUser color="rgb(255, 191, 116)" size={22} />
-                <a href={`https://github.com/${login}`}>{login}</a>
+                <Tooltip text='Github username'>
+                  <FaUser color="rgb(255, 191, 116)" size={22} />
+                  <a href={`https://github.com/${login}`}>{login}</a>
+                </Tooltip>
               </li>
               <li>
                 <FaStar color="rgb(255, 215, 0)" size={22} />
--- app/components/Results.js
+++ app/components/Results.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import Loading from './Loading';
 import { battle } from '../utils/api';
 
+import Tooltip from './Tooltip';
 import Card from './Card';
 
 import {
@@ -24,14 +25,18 @@ function ProfileList({ profile }) {
       </li>
       {profile.location && (
         <li>
-          <FaCompass color="rgb(144, 115, 255)" size={22} />
-          {profile.location}
+          <Tooltip text="User's Location">
+            <FaCompass color="rgb(144, 115, 255)" size={22} />
+            {profile.location}
+          </Tooltip>
         </li>
       )}
       {profile.company && (
         <li>
-          <FaBriefcase color="#795548" size={22} />
-          {profile.company}
+          <Tooltip text="User's company">
+            <FaBriefcase color='#795548' size={22} />
+            {profile.company}
+          </Tooltip>
         </li>
       )}
       <li>
